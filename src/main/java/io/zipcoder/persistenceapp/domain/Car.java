f5011ccf0261e018d16de4a254c11aa5c1e361c3
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2017-09-12 13:46:25 -0400

    Configure entities

--- src/main/java/io/zipcoder/persistenceapp/domain/Car.java
+++ src/main/java/io/zipcoder/persistenceapp/domain/Car.java
@@ -1,5 +1,7 @@
 package io.zipcoder.persistenceapp.domain;
 
+
+import javax.persistence.*;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,12 +9,23 @@ import java.util.List;
 /**
  *
  */
+@Entity
+@Table(name = "Cars") // Required if class & table names are different
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int Id;
 
     private String make;
     private String model;
     private String year;
+
+    @OneToMany
     private List<SalesPackage> salesPackages = new LinkedList<>();
+
+    public Car() {
+    }
 
     public Car(String make, String model, String year) {
         this.make = make;
@@ -49,7 +62,15 @@ public class Car {
         salesPackages.add(sp);
     }
 
-    public Iterable<SalesPackage> getSalesPackages(){
+    public List<SalesPackage> getSalesPackages(){
         return Collections.unmodifiableList(salesPackages);
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
     }
 }
--- src/main/java/io/zipcoder/persistenceapp/domain/SalesPackage.java
+++ src/main/java/io/zipcoder/persistenceapp/domain/SalesPackage.java
@@ -1,14 +1,30 @@
 package io.zipcoder.persistenceapp.domain;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
  *
  */
+@Entity
+@Table(name = "auto_prices")
 public class SalesPackage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
 
     private String pkgName;
     private BigDecimal price;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getPkgName() {
         return pkgName;
